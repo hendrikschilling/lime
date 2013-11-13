dfe744c3ad997a2d922e6154cbc8fdb0059efdea
Hendrik Siedelmann <hendrik.siedelmann@gmail.com>
2013-11-13 14:20:36 +0700

    add half quality idcts

--- src/lib/ujpeg.c
+++ src/lib/ujpeg.c
@@ -179,6 +179,51 @@ UJ_INLINE void ujRowIDCT(int* blk) {
     blk[7] = (x7 - x1) >> 8;
 }
 
+UJ_INLINE void ujRowIDCT_half(int* blk) {
+    int x0, x1, x2, x3, x4, x5, x6, x7, x8;
+    if (!((x1 = 0 << 11)
+        | (x2 = 0)
+        | (x3 = blk[2])
+        | (x4 = blk[1])
+        | (x5 = 0)
+        | (x6 = 0)
+        | (x7 = blk[3])))
+    {
+        blk[0] = blk[1] = blk[2] = blk[3] = blk[4] = blk[5] = blk[6] = blk[7] = blk[0] << 3;
+        return;
+    }
+    x0 = (blk[0] << 11) + 128;
+    x8 = W7 * (x4 + 0);
+    x4 = x8 + (W1 - W7) * x4;
+    x5 = 0; 
+    x8 = W3 * (x6 + x7);
+    x6 = x8 - (W3 - W5) * x6;
+    x7 = x8 - (W3 + W5) * x7;
+    x8 = x0 + x1;
+    x0 -= x1;
+    x1 = W6 * (x3 + x2);
+    x2 = x1 - (W2 + W6) * x2;
+    x3 = x1 + (W2 - W6) * x3;
+    x1 = x4 + x6;
+    x4 -= x6;
+    x6 = x5 + x7;
+    x5 -= x7;
+    x7 = x8 + x3;
+    x8 -= x3;
+    x3 = x0 + x2;
+    x0 -= x2;
+    x2 = (181 * (x4 + x5) + 128) >> 8;
+    x4 = (181 * (x4 - x5) + 128) >> 8;
+    blk[0] = (x7 + x1) >> 8;
+    blk[1] = (x3 + x2) >> 8;
+    blk[2] = (x0 + x4) >> 8;
+    blk[3] = (x8 + x6) >> 8;
+    blk[4] = (x8 - x6) >> 8;
+    blk[5] = (x0 - x4) >> 8;
+    blk[6] = (x3 - x2) >> 8;
+    blk[7] = (x7 - x1) >> 8;
+}
+
 UJ_INLINE void ujColIDCT(const int* blk, unsigned char *out, int stride) {
     int x0, x1, x2, x3, x4, x5, x6, x7, x8;
     if (!((x1 = blk[8*4] << 8)
@@ -207,6 +252,55 @@ UJ_INLINE void ujColIDCT(const int* blk, unsigned char *out, int stride) {
     x0 -= x1;
     x1 = W6 * (x3 + x2) + 4;
     x2 = (x1 - (W2 + W6) * x2) >> 3;
+    x3 = (x1 + (W2 - W6) * x3) >> 3;
+    x1 = x4 + x6;
+    x4 -= x6;
+    x6 = x5 + x7;
+    x5 -= x7;
+    x7 = x8 + x3;
+    x8 -= x3;
+    x3 = x0 + x2;
+    x0 -= x2;
+    x2 = (181 * (x4 + x5) + 128) >> 8;
+    x4 = (181 * (x4 - x5) + 128) >> 8;
+    *out = ujClip(((x7 + x1) >> 14) + 128);  out += stride;
+    *out = ujClip(((x3 + x2) >> 14) + 128);  out += stride;
+    *out = ujClip(((x0 + x4) >> 14) + 128);  out += stride;
+    *out = ujClip(((x8 + x6) >> 14) + 128);  out += stride;
+    *out = ujClip(((x8 - x6) >> 14) + 128);  out += stride;
+    *out = ujClip(((x0 - x4) >> 14) + 128);  out += stride;
+    *out = ujClip(((x3 - x2) >> 14) + 128);  out += stride;
+    *out = ujClip(((x7 - x1) >> 14) + 128);
+}
+
+UJ_INLINE void ujColIDCT_half(const int* blk, unsigned char *out, int stride) {
+    int x0, x1, x2, x3, x4, x5, x6, x7, x8;
+    if (!((x1 = 0 << 8)
+        | (x2 = 0)
+        | (x3 = blk[8*2])
+        | (x4 = blk[8*1])
+        | (x5 = 0)
+        | (x6 = 0)
+        | (x7 = blk[8*3])))
+    {
+        x1 = ujClip(((blk[0] + 32) >> 6) + 128);
+        for (x0 = 8;  x0;  --x0) {
+            *out = (unsigned char) x1;
+            out += stride;
+        }
+        return;
+    }
+    x0 = (blk[0] << 8) + 8192;
+    x8 = W7 * (x4 + 0) + 4;
+    x4 = (x8 + (W1 - W7) * x4) >> 3;
+    x5 = (x8 - (W1 + W7) * 0) >> 3;
+    x8 = W3 * (0 + x7) + 4;
+    x6 = (x8 - (W3 - W5) * 0) >> 3;
+    x7 = (x8 - (W3 + W5) * x7) >> 3;
+    x8 = x0 + 0;
+    x0 -= 0;
+    x1 = W6 * (x3 + 0) + 4;
+    x2 = (x1 - (W2 + W6) * 0) >> 3;
     x3 = (x1 + (W2 - W6) * x3) >> 3;
     x1 = x4 + x6;
     x4 -= x6;
