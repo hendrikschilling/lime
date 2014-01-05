487e629a1ed67b82b48f65a2244f4bfe0ff86c68
Hendrik Siedelmann <hendrik.siedelmann@gmail.com>
2014-01-05 16:04:16 +1300

    add scale manipulation to assert filter

--- src/lib/filter_assert.c
+++ src/lib/filter_assert.c
@@ -1,11 +1,14 @@
 #include "filter_assert.h"
 
 typedef struct {
+  Meta *dim_in_meta;
+  Dim *out_dim;
   Meta *color_in[3], *color_out[3];
   int in_cs, out_cs;
+  int scale;
 } _Data;
 
-int _setting_changed(Filter *f)
+static int _setting_changed(Filter *f)
 {
   _Data *data = ea_data(f->data, 0);
   
@@ -62,6 +65,23 @@ int _setting_changed(Filter *f)
   return 0;
 }
 
+static int _input_fixed(Filter *f)
+{
+  _Data *data = ea_data(f->data, 0);
+  Dim *in_dim = data->dim_in_meta->data;
+  
+  assert(!in_dim->x);
+  assert(!in_dim->y);
+  
+  data->out_dim->x = 0;
+  data->out_dim->y = 0;
+  data->out_dim->width = in_dim->width / (1u << data->scale);
+  data->out_dim->height = in_dim->height / (1u << data->scale);
+  data->out_dim->scaledown_max = in_dim->scaledown_max - data->scale;
+  
+  return 0;
+}
+
 static void _worker(Filter *f, Eina_Array *in, Eina_Array *out, Rect *area, int thread_id)
 {
   int ch;
@@ -74,20 +94,40 @@ static void _worker(Filter *f, Eina_Array *in, Eina_Array *out, Rect *area, int 
   }
 }
 
+static void _area_calc(Filter *f, Rect *in, Rect *out)
+{
+  _Data *data = ea_data(f->data, 0);
+  
+  out->corner.scale = in->corner.scale + data->scale;
+  out->corner.x = in->corner.x;
+  out->corner.y = in->corner.y;
+  out->width = in->width; 
+  out->height = in->height;
+
+}
+
 Filter *filter_assert_new(void)
 {
   Filter *filter = filter_new(&filter_core_assert);
-  Meta *in, *out, *channel, *bitdepth, *setting, *bound;
+  Meta *in, *out, *channel, *bitdepth, *setting, *bound, *size_in, *size_out;
   Meta *ch_out[3];
   filter->fixme_outcount = 3;
   _Data *data = calloc(sizeof(_Data), 1);
   data->out_cs = 1;
+  data->out_dim = calloc(sizeof(Dim), 1);
   ea_push(filter->data, data);
   filter->setting_changed = &_setting_changed;
+  filter->input_fixed = &_input_fixed;
   
   filter->mode_buffer = filter_mode_buffer_new();
   filter->mode_buffer->worker = &_worker;
   filter->mode_buffer->threadsafe = 1;
+  filter->mode_buffer->area_calc = &_area_calc;
+  
+  size_in = meta_new(MT_IMGSIZE, filter);
+  size_out = meta_new_data(MT_IMGSIZE, filter, data->out_dim);
+  data->dim_in_meta = size_in;
+  size_in->replace = size_out;
   
   bitdepth = meta_new_data(MT_BITDEPTH, filter, malloc(sizeof(int)));
   *(int*)(bitdepth->data) = BD_U8;
@@ -100,6 +140,7 @@ Filter *filter_assert_new(void)
   data->color_out[0] = meta_new_data(MT_COLOR, filter, malloc(sizeof(int)));
   *(int*)(data->color_out[0]->data) = CS_LAB_L;
   meta_attach(channel, data->color_out[0]);
+  meta_attach(channel, size_out);
   meta_attach(channel, bitdepth);
   meta_attach(out, channel);
   ch_out[0] = channel;
@@ -108,6 +149,7 @@ Filter *filter_assert_new(void)
   data->color_out[1] = meta_new_data(MT_COLOR, filter, malloc(sizeof(int)));
   *(int*)(data->color_out[1]->data) = CS_LAB_A;
   meta_attach(channel, data->color_out[1]);
+  meta_attach(channel, size_out);
   meta_attach(channel, bitdepth);
   meta_attach(out, channel);
   ch_out[1] = channel;
@@ -116,6 +158,7 @@ Filter *filter_assert_new(void)
   data->color_out[2] = meta_new_data(MT_COLOR, filter, malloc(sizeof(int)));
   *(int*)(data->color_out[2]->data) = CS_LAB_B;
   meta_attach(channel, data->color_out[2]);
+  meta_attach(channel, size_out);
   meta_attach(channel, bitdepth);
   meta_attach(out, channel);
   ch_out[2] = channel;
@@ -131,6 +174,7 @@ Filter *filter_assert_new(void)
   channel->replace = ch_out[0];
   meta_attach(channel, data->color_in[0]);
   meta_attach(channel, bitdepth);
+  meta_attach(channel, size_in);
   meta_attach(in, channel);
   
   channel = meta_new_channel(filter, 2);
@@ -140,6 +184,7 @@ Filter *filter_assert_new(void)
   channel->replace = ch_out[1];
   meta_attach(channel, data->color_in[1]);
   meta_attach(channel, bitdepth);
+  meta_attach(channel, size_in);
   meta_attach(in, channel);
   
   channel = meta_new_channel(filter, 3);
@@ -149,6 +194,7 @@ Filter *filter_assert_new(void)
   channel->replace = ch_out[2];
   meta_attach(channel, data->color_in[2]);
   meta_attach(channel, bitdepth);
+  meta_attach(channel, size_in);
   meta_attach(in, channel);
   
   setting = meta_new_data(MT_INT, filter, &data->in_cs);
@@ -178,6 +224,10 @@ Filter *filter_assert_new(void)
   *(int*)bound->data = 3;
   meta_name_set(bound, "PARENT_SETTING_MAX");
   meta_attach(setting, bound);
+  
+  setting = meta_new_data(MT_INT, filter, &data->scale);
+  meta_name_set(setting, "scale"); 
+  eina_array_push(filter->settings, setting);
   
   return filter;
 }
